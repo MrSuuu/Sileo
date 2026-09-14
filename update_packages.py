bd07efedf2d327468885168f09f3632bec659452
苏先生 <547250119@qq.com>
2026-09-14 17:02:37 +0800

    描述页改用 muzikeji 兼容格式（DepictionTabView + TableTextView）；修复字段名 Sileodepiction（小写d）

--- update_packages.py
+++ update_packages.py
@@ -176,9 +176,9 @@ def build_packages_text(debs_dir: Path) -> str:
             info = parse_deb_control(deb)
             stem = info.get('Package','?').split('.')[-1]
             info['Depiction']       = f'https://MrSuuu.github.io/Sileo/depictions/{stem}.html'
-            info['SileoDepiction']  = f'https://MrSuuu.github.io/Sileo/depictions/{stem}.json'
+            info['Sileodepiction']  = f'https://MrSuuu.github.io/Sileo/depictions/{stem}.json'
             for key in ["Package", "Name", "Version", "Architecture", "Description",
-                        "Maintainer", "Author", "Depiction", "SileoDepiction", "Section", "Depends",
+                        "Maintainer", "Author", "Depiction", "Sileodepiction", "Section", "Depends",
                         "Filename", "Size"]:
                 if key in info:
                     lines.append(f"{key}: {info[key]}")
